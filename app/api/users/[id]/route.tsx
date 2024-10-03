62bdf24a1f37c2023ff44f7511500c66c0938726
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-03 14:22:25 +0700

    Add : validate body using zod

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(
   request: NextRequest,
@@ -17,13 +18,12 @@ export async function PUT(
   request: NextRequest,
   { params }: { params: { id: number } },
 ) {
-  // Validate the request body
   const body = await request.json();
-  // If invalid, return 400
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
 
-  // Fetch the user with the given id
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 // GET - getting data
 export function GET(request: NextRequest) {
@@ -14,9 +15,10 @@ export function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const body = await request.json();
   // Validate
-  if (!body.name)
+  const validation = schema.safeParse(body);
+  if (!validation.success)
     // If invalid, return 400
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+    return NextResponse.json(validation.error.errors, { status: 400 });
   // Else, return data
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
