09722c68b36a5dedf2cc00a9cea46b819b672037
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-04 14:56:43 +0700

    Add : access user session in client component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,21 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex space-x-5 bg-slate-200 p-3">
       <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
-      <Link href="/api/auth/signin">Login</Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
