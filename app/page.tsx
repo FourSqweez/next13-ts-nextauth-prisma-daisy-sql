e4e8e715217aa7a9e3313b31bf017d5293320788
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-05 13:17:02 +0700

    Add : try image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import grape from "@/public/images/grape.png";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  console.log({ session });
   return (
-    <main>
-      <h1>
-        Hello{" "}
-        {session && (
-          <span>
-            {session.user?.name ? session.user?.name : session.user?.email}
-          </span>
-        )}
-      </h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="grape"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={75}
+        priority
+      />
     </main>
   );
 }
