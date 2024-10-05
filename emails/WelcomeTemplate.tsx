8777942d7250fd508bbaaf442beb3d197c47de11
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-05 12:32:51 +0700

    Add : style component with tailwind

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,18 +6,21 @@ import {
   Text,
   Link,
   Preview,
+  Tailwind,
 } from "@react-email/components";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aboard!</Preview>
-      <Body style={body}>
-        <Container>
-          <Text style={heading}>Hello {name}</Text>
-          <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="text-3xl font-bold">Hello {name}</Text>
+            <Link href="https://codewithmosh.com">www.codewithmosh.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
